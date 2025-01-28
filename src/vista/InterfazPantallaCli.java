a0969cfd7e6bde2751644d438e1f25e553424e89
emorio00 <emorio00@estudiantes.unileon.es>
2025-01-28 11:30:00 +0100

    cambio en el boton usuario

--- src/vista/InterfazPantallaCli.java
+++ src/vista/InterfazPantallaCli.java
@@ -1,14 +1,11 @@
 package vista;
 
 import java.awt.Dimension;
-import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.GridBagLayout;
 import java.awt.Toolkit;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.GridBagConstraints;
@@ -24,65 +21,68 @@ public class InterfazPantallaCli extends JFrame {
 	 */
 	public InterfazPantallaCli() {
 		// Configura el título de la ventana
-        super("Pantalla Principal");
-        setFont(new Font("Dialog", Font.BOLD, 15));
+      super("Pantalla Principal");
+      setFont(new Font("Dialog", Font.BOLD, 15));
 
-        // Configura la operación al cerrar la ventana
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      // Configura la operación al cerrar la ventana
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        // Configura el tamaño de la ventana
-        Toolkit toolkit = Toolkit.getDefaultToolkit();
-        Dimension pantalla = toolkit.getScreenSize();
-        int width = (int) pantalla.getWidth();
-        int height = (int) pantalla.getHeight();
+      // Configura el tamaño de la ventana
+      Toolkit toolkit = Toolkit.getDefaultToolkit();
+      Dimension pantalla = toolkit.getScreenSize();
+      int width = (int) pantalla.getWidth();
+      int height = (int) pantalla.getHeight();
         
-        int part = height / 3;
+      int part = height / 3;
         
-        setSize(pantalla);
+      setSize(pantalla);
 
-        GridBagLayout gridBagLayout = new GridBagLayout();
-        gridBagLayout.columnWidths = new int[]{width/4, width/3, width/4};
-        gridBagLayout.rowHeights = new int[]{part-part/5, part/2, part/6, part/2, part-part/5};
-        gridBagLayout.columnWeights = new double[]{0.0, Double.MIN_VALUE};
-        gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0};
-        getContentPane().setLayout(gridBagLayout);
+      GridBagLayout gridBagLayout = new GridBagLayout();
+      gridBagLayout.columnWidths = new int[]{width/4, width/3, width/4};
+      gridBagLayout.rowHeights = new int[]{part-part/5, part/2, part/6, part/2, part-part/5};
+      gridBagLayout.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+      gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0};
+      getContentPane().setLayout(gridBagLayout);
         
-        JButton btnUsuario = new JButton("Usuario");
-        btnUsuario.setBackground(new Color(0, 128, 128));
-        btnUsuario.setForeground(Color.BLACK);
-        btnUsuario.setFont(new Font("Dialog", Font.BOLD, 17));
-        GridBagConstraints gbc_btnUsuario = new GridBagConstraints();
-        gbc_btnUsuario.anchor = GridBagConstraints.NORTHEAST;
-        gbc_btnUsuario.insets = new Insets(0, 0, 5, 0);
-        gbc_btnUsuario.gridx = 2;
-        gbc_btnUsuario.gridy = 0;
-        getContentPane().add(btnUsuario, gbc_btnUsuario);
+      JButton btnUsuario = new JButton("Usuario");
+      btnUsuario.setBackground(new Color(0, 128, 128));
+      btnUsuario.setForeground(Color.BLACK);
+      btnUsuario.setFont(new Font("Dialog", Font.BOLD, 17));
+      btnUsuario.setPreferredSize(new Dimension(150, 50));
+      btnUsuario.setMinimumSize(new Dimension(150, 50));
+      btnUsuario.setMaximumSize(new Dimension(150, 50));
+      GridBagConstraints gbc_btnUsuario = new GridBagConstraints();
+      gbc_btnUsuario.anchor = GridBagConstraints.NORTHEAST;
+      gbc_btnUsuario.insets = new Insets(0, 0, 5, 0);
+      gbc_btnUsuario.gridx = 2;
+      gbc_btnUsuario.gridy = 0;
+      getContentPane().add(btnUsuario, gbc_btnUsuario);
         
-        JButton btnDevolverProductos = new JButton("Devolver Productos");
-        btnDevolverProductos.setFont(new Font("Dialog", Font.BOLD, 25));
-        btnDevolverProductos.setBackground(new Color(0, 128, 128));
-        btnDevolverProductos.setForeground(Color.BLACK);
-        GridBagConstraints gbc_btnDevolverProductos = new GridBagConstraints();
-        gbc_btnDevolverProductos.fill = GridBagConstraints.BOTH;
-        gbc_btnDevolverProductos.insets = new Insets(0, 0, 5, 5);
-        gbc_btnDevolverProductos.gridx = 1;
-        gbc_btnDevolverProductos.gridy = 1;
-        getContentPane().add(btnDevolverProductos, gbc_btnDevolverProductos);
+      JButton btnDevolverProductos = new JButton("Devolver Productos");
+      btnDevolverProductos.setFont(new Font("Dialog", Font.BOLD, 25));
+      btnDevolverProductos.setBackground(new Color(0, 128, 128));
+      btnDevolverProductos.setForeground(Color.BLACK);
+      GridBagConstraints gbc_btnDevolverProductos = new GridBagConstraints();
+      gbc_btnDevolverProductos.fill = GridBagConstraints.BOTH;
+      gbc_btnDevolverProductos.insets = new Insets(0, 0, 5, 5);
+      gbc_btnDevolverProductos.gridx = 1;
+      gbc_btnDevolverProductos.gridy = 1;
+      getContentPane().add(btnDevolverProductos, gbc_btnDevolverProductos);
         
-        JButton btnSacarProductos = new JButton("Sacar Productos");
-        btnSacarProductos.setForeground(Color.BLACK);
-        btnSacarProductos.setBackground(new Color(0, 128, 128));
-        btnSacarProductos.setFont(new Font("Dialog", Font.BOLD, 25));
-        GridBagConstraints gbc_btnSacarProductos = new GridBagConstraints();
-        gbc_btnSacarProductos.fill = GridBagConstraints.BOTH;
-        gbc_btnSacarProductos.insets = new Insets(0, 0, 5, 5);
-        gbc_btnSacarProductos.gridx = 1;
-        gbc_btnSacarProductos.gridy = 3;
-        getContentPane().add(btnSacarProductos, gbc_btnSacarProductos);
+      JButton btnSacarProductos = new JButton("Sacar Productos");
+      btnSacarProductos.setForeground(Color.BLACK);
+      btnSacarProductos.setBackground(new Color(0, 128, 128));
+      btnSacarProductos.setFont(new Font("Dialog", Font.BOLD, 25));
+      GridBagConstraints gbc_btnSacarProductos = new GridBagConstraints();
+      gbc_btnSacarProductos.fill = GridBagConstraints.BOTH;
+      gbc_btnSacarProductos.insets = new Insets(0, 0, 5, 5);
+      gbc_btnSacarProductos.gridx = 1;
+      gbc_btnSacarProductos.gridy = 3;
+      getContentPane().add(btnSacarProductos, gbc_btnSacarProductos);
         
      // Establecer el icono de la ventana
-        ImageIcon icon = new ImageIcon(getClass().getResource("../imagenes/logo.png"));
-        setIconImage(icon.getImage());
+      ImageIcon icon = new ImageIcon(getClass().getResource("../imagenes/logo.png"));
+      setIconImage(icon.getImage());
 	}
 
 }
