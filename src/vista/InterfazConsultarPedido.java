93e667c5f8ffe634268a8d984cd6ea530e878edf
emorio00 <emorio00@estudiantes.unileon.es>
2025-01-31 15:03:33 +0100

    cambios de la interfaz consultar pedido

--- src/vista/InterfazConsultarPedido.java
+++ src/vista/InterfazConsultarPedido.java
@@ -6,11 +6,13 @@ import java.awt.GridBagLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
@@ -29,38 +31,26 @@ public class InterfazConsultarPedido extends JFrame {
 	 * Create the frame.
 	 */
 	public InterfazConsultarPedido() {
-		// Configura el título de la ventana
-		super("Consultar Pedido");
-		setFont(new Font("Dialog", Font.BOLD, 15));
+		// Quita los botones de cerrar, minimizar y maximizar
+        setUndecorated(true); 
+        
+     	// Configura el tamaño de la ventana
+      	Toolkit toolkit = Toolkit.getDefaultToolkit();
+      	Dimension pantalla = toolkit.getScreenSize();
+      	int width = (int) pantalla.getWidth();
+      	int height = (int) pantalla.getHeight();
 
-		// Configura la operación al cerrar la ventana
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			
-		// Configura el tamaño de la ventana
-		Toolkit toolkit = Toolkit.getDefaultToolkit();
-		Dimension pantalla = toolkit.getScreenSize();
-		int width = (int) pantalla.getWidth();
-		int height = (int) pantalla.getHeight();
+      	int partH = height / 3;
+		int partW = width / 3;
+       
+      	setSize(pantalla);
 
-		Insets bordes = getInsets();
-        int top = bordes.top;
-        int bottom = bordes.bottom;
-        int left = bordes.left;
-        int right = bordes.right;
-
-        height = height - top - bottom;
-        width = width - left - right;
-			
-		int part = height / 3;
-		
-		setSize(pantalla);
-
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{width/4, width/3, width/4};
-		gridBagLayout.rowHeights = new int[]{part,part/4, part/4, part/2, part};
-		gridBagLayout.columnWeights = new double[]{0.0, 1.0};
-		gridBagLayout.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0};
-		getContentPane().setLayout(gridBagLayout);
+      	GridBagLayout gridBagLayout = new GridBagLayout();
+      	gridBagLayout.columnWidths = new int[]{partW/2, partW, partW, partW/2};
+      	gridBagLayout.rowHeights = new int[]{partH/8, partH/4, partH/6, partH/2, partH-partH/12};
+      	gridBagLayout.columnWeights = new double[]{0.0, 1.0};
+      	gridBagLayout.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 1.0};
+      	getContentPane().setLayout(gridBagLayout);
 
 		JButton btnVolver = new JButton("< Volver");
 		btnVolver.addActionListener(new ActionListener() {
@@ -83,20 +73,6 @@ public class InterfazConsultarPedido extends JFrame {
 		gbc_btnVolver.gridx = 0;
 		gbc_btnVolver.gridy = 0;
 		getContentPane().add(btnVolver, gbc_btnVolver);
-			
-		JButton btnUsuario = new JButton("Usuario");
-		btnUsuario.setBackground(new Color(0, 128, 128));
-		btnUsuario.setForeground(Color.BLACK);
-		btnUsuario.setFont(new Font("Dialog", Font.BOLD, 17));
-		btnUsuario.setPreferredSize(new Dimension(150, 50));
-		btnUsuario.setMinimumSize(new Dimension(150, 50));
-		btnUsuario.setMaximumSize(new Dimension(150, 50));
-		GridBagConstraints gbc_btnUsuario = new GridBagConstraints();
-		gbc_btnUsuario.anchor = GridBagConstraints.NORTHEAST;
-		gbc_btnUsuario.insets = new Insets(0, 0, 5, 0);
-		gbc_btnUsuario.gridx = 2;
-		gbc_btnUsuario.gridy = 0;
-		getContentPane().add(btnUsuario, gbc_btnUsuario);
 
 		JPopupMenu menuUsuario = new JPopupMenu();
 		JMenuItem itemNombre = new JMenuItem(getName()); //Como obtiene el nombre de usuario??
@@ -118,10 +94,31 @@ public class InterfazConsultarPedido extends JFrame {
 				dispose(); // Cierra la ventana actual
 			}
 		});
+
 		menuUsuario.add(btnCerrarSesion);
 		menuUsuario.setPreferredSize(new Dimension(150, 100));
 		menuUsuario.setMinimumSize(new Dimension(150, 100));
 		menuUsuario.setMaximumSize(new Dimension(1500, 100));
+		
+		JButton btnUsuario = new JButton("Usuario");
+		btnUsuario.setBackground(new Color(0, 128, 128));
+		btnUsuario.setForeground(Color.BLACK);
+		btnUsuario.setFont(new Font("Dialog", Font.BOLD, 17));
+		btnUsuario.setPreferredSize(new Dimension(150, 50));
+		btnUsuario.setMinimumSize(new Dimension(150, 50));
+		btnUsuario.setMaximumSize(new Dimension(150, 50));
+		GridBagConstraints gbc_btnUsuario = new GridBagConstraints();
+		gbc_btnUsuario.anchor = GridBagConstraints.NORTHEAST;
+		gbc_btnUsuario.insets = new Insets(0, 0, 5, 5);
+		gbc_btnUsuario.gridx = 3;
+		gbc_btnUsuario.gridy = 0;
+		getContentPane().add(btnUsuario, gbc_btnUsuario);
+		
+				btnUsuario.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						menuUsuario.show(btnUsuario, btnUsuario.getWidth(), btnUsuario.getHeight());
+					}
+				});
 
 		txtBuscarNombre = new JTextField();
 		txtBuscarNombre.setFont(new Font("Dialog", Font.PLAIN, 25));
@@ -150,9 +147,10 @@ public class InterfazConsultarPedido extends JFrame {
         lblErrorNoAlquilar.setForeground(Color.RED);
         lblErrorNoAlquilar.setVisible(false);
         GridBagConstraints gbc_lblErrorNoAlquilar = new GridBagConstraints();
+        gbc_lblErrorNoAlquilar.anchor = GridBagConstraints.SOUTH;
         gbc_lblErrorNoAlquilar.insets = new Insets(0, 0, 5, 5);
         gbc_lblErrorNoAlquilar.gridx = 1;
-        gbc_lblErrorNoAlquilar.gridy = 8;
+        gbc_lblErrorNoAlquilar.gridy = 2;
         getContentPane().add(lblErrorNoAlquilar, gbc_lblErrorNoAlquilar);
         
 		JButton btnBuscar = new JButton("Buscar");
@@ -164,8 +162,8 @@ public class InterfazConsultarPedido extends JFrame {
       	btnBuscar.setMaximumSize(new Dimension(150, 50));
 		GridBagConstraints gbc_btnBuscar = new GridBagConstraints();
 		gbc_btnBuscar.insets = new Insets(0, 0, 5, 5);
-		gbc_btnBuscar.gridx = 1;
-		gbc_btnBuscar.gridy = 3;
+		gbc_btnBuscar.gridx = 2;
+		gbc_btnBuscar.gridy = 1;
 		getContentPane().add(btnBuscar, gbc_btnBuscar);
 
 		btnBuscar.addActionListener(new ActionListener() {
@@ -180,10 +178,35 @@ public class InterfazConsultarPedido extends JFrame {
 					boolean alquilado = controladorBuscarUsuario.buscarAlquiler(nombre);
 					if(alquilado) {
 						lblErrorNoAlquilar.setVisible(false);
+						txtBuscarNombre.setVisible(false);
+						btnBuscar.setVisible(false);
 						
-						controladorBuscarUsuario.obtenerProductos(nombre);
-						//
+						JLabel lblnombreCliente = new JLabel("Usuario: " + nombre);
+						lblnombreCliente.setFont(new Font("Dialog", Font.BOLD, 20));
+						lblnombreCliente.setForeground(Color.BLACK);
+						lblnombreCliente.setVisible(true);
+						GridBagConstraints gbc_lblnombreCliente= new GridBagConstraints();
+						gbc_lblnombreCliente.anchor = GridBagConstraints.SOUTH;
+						gbc_lblnombreCliente.insets = new Insets(0, 0, 5, 5);
+						gbc_lblnombreCliente.gridx = 1;
+						gbc_lblnombreCliente.gridy = 1;
+						getContentPane().add(lblnombreCliente, gbc_lblnombreCliente);
+
+						JLabel productosCliente = new JLabel("\nProductos a Alquilar:\n");
+						productosCliente.setFont(new Font("Dialog", Font.BOLD, 20));
+						productosCliente.setForeground(Color.BLACK);
+						productosCliente.setVisible(true);
+						GridBagConstraints gbc_productosCliente = new GridBagConstraints();
+						gbc_productosCliente.anchor = GridBagConstraints.SOUTH;
+						gbc_productosCliente.insets = new Insets(0, 0, 5, 5);
+						gbc_productosCliente.gridx = 1;
+						gbc_productosCliente.gridy = 2;
+						getContentPane().add(productosCliente, gbc_productosCliente);
+
+						ArrayList<String> productos = controladorBuscarUsuario.obtenerProductos(nombre);
+
 						//MOSTRAR EL PEDIDO
+						//LOS PRODUCTOS DEL CLIENTE QUE SE BUSCAN SON LOS DEL ARRAYlIST
 					}
 					else {
 						//Mostrar mensaje de error
@@ -198,12 +221,10 @@ public class InterfazConsultarPedido extends JFrame {
 				}
 			}
 		});
-
-		btnUsuario.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				menuUsuario.show(btnUsuario, btnUsuario.getWidth(), btnUsuario.getHeight());
-			}
-		});
+		
+		// Establecer el icono de la ventana
+	     ImageIcon icon = new ImageIcon(getClass().getResource("../imagenes/logo.png"));
+	     setIconImage(icon.getImage());
 	}
 	
 }
